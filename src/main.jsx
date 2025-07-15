26db0c58debf0a3de0877bc7c590639e3e01b160
Arsenegpt <arsenegpt@gmail.com>
2025-07-15 17:08:05 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Catalogo from './pages/Catalogo';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Catalogo />
+  </React.StrictMode>
+);
